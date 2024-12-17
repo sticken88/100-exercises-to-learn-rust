be4751029ed811fefc40c67f5541dd8fc75b619f
Matteo Sticco <>
2024-12-17 15:33:13 +0100

    Implement solution to ex 08 to 12 from chap 03

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -6,6 +6,76 @@
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
 //
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        validate_product_name(&product_name);
+        validate_quantity(quantity);
+        validate_unit_price(unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        validate_product_name(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        validate_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        validate_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
+
+fn validate_product_name(product_name: &String) {
+    if product_name.is_empty() || product_name.len() > 300 {
+        panic!("Product name cannot be empty or longer than 200 bytes!");
+    }
+}
+
+fn validate_quantity(quantity: u32) {
+    if quantity <= 0 {
+        panic!("Quantity must be positive!");
+    }
+}
+
+fn validate_unit_price(unit_price: u32) {
+    if unit_price <= 0 {
+        panic!("Unit price must be positive!");
+    }
+}
+
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
 // Integration here has a very specific meaning: they test **the public API** of your project.
